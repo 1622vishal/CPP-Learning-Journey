42293eb3b94d4cc0940c5255b3fbd771862090f1
Vishal Yadav <vishalyadav82738@gmail.com>
2023-09-28 17:01:47 +0530

    Added dangling pointers

--- DynamicMA.cpp
+++ DynamicMA.cpp
@@ -9,9 +9,12 @@ int main()
     int* ptr= new int ;   //Variable allocation
     int* ptr2= new int[5];  //Array allocation
     int* ptr3= new int(74);   //Initialisation
+    int* ptr4 =new int(457);
     cout<<ptr<<endl;
     cout<<*ptr<<endl;
     cout<<*ptr3<<endl;
+    cout<<*ptr4<<endl;
+
 
     /*Deleting the dynamically allocated memory*/
 
@@ -20,5 +23,12 @@ int main()
     delete ptr3;
     cout<<*ptr3<<endl;
     cout<<ptr3;
+
+    /*Dangling Pointers*/
+    /*Dangling pointers occur when a pointer continues to reference a memory location that has been deallocated (freed) or has gone out of scope. To prevent dangling pointers initialise pointer with NULL.*/
+    delete ptr4;
+    ptr4 = NULL;
+    cout<<*ptr4;        //No Output
+
     return 0;
 }
