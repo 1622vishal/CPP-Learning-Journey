8a8870707e61e271f183fc293fe82272f45d9d86
Vishal Yadav <vishalyadav82738@gmail.com>
2023-10-06 15:45:52 +0530

    Added StringParseWords

---
+++ StringParseWords.cpp
@@ -0,0 +1,20 @@
+/*----Written by :- Vishal Yadav----*/
+/*----Date :- 06-10-23----*/
+
+/*Program to demonstrate the use of getline to parse words*/
+
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"Enter any sentence to parse it."<<endl;
+    while(cin>>str)
+    {
+        cout<<str<<endl;
+    }
+    return 0;
+}
