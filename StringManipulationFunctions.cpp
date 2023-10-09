e174f09198b160894563f4234938a0e71a8ddcbf
Vishal Yadav <vishalyadav82738@gmail.com>
2023-10-09 23:10:37 +0530

    Added StringManipulationFunctions.cpp

---
+++ StringManipulationFunctions.cpp
@@ -0,0 +1,21 @@
+/*----Written by :- Vishal Yadav----*/
+/*----Date :- 09-10-23----*/
+
+/*Program to demonstrate the functions used with strings*/
+
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    //String Length Function
+    string str = "Hello";
+    cout<<str.length()<<endl;      
+    cout<<str.size()<<endl;
+    //Note:- strlen() is used with char arrays
+
+    
+    return 0;
+}
